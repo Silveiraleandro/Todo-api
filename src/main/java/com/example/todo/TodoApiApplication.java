c24e7f44a2ec45f398474e952a61dc66da282761
Leandro Silveira <leovoz@yahoo.com.br>
2025-08-16 19:27:38 -0300

    Adding the first lines. Adding spring dependencies to pom, packages and TodoApplication

---
+++ src/main/java/com/example/todo/TodoApiApplication.java
@@ -0,0 +1,10 @@
+package com.example.todo;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class TodoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+}
